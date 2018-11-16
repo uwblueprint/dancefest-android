edc0e1f40be1fd9487c2952de9b4b42628b729b1
Sophie Qin <sophieqin@tdlab.io>
2018-11-15 19:00:01 -0500

    inital design for adjudication

--- app/src/main/java/com/uwblueprint/dancefest/CritiqueFormActivity.kt
+++ app/src/main/java/com/uwblueprint/dancefest/CritiqueFormActivity.kt
@@ -8,31 +8,5 @@ class CritiqueFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_critique_form)
-        fetchEntryData(1.0)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        loadEntry()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        saveEntry()
-    }
-
-    private fun fetchEntryData(id: Double) {
-
-
-    }
-
-    private fun loadEntry() {
-        // loads the Entry
-
-    }
-
-    private fun saveEntry() {
-        // saves the the current data into Database
-
     }
 }
