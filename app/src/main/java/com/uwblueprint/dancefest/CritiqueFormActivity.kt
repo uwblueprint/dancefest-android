0751cae2012eb4daa7a7285831d6d56f0343b463
Sophie Qin <sophieqin@tdlab.io>
2018-11-15 19:00:01 -0500

    inital design for adjudication

---
+++ app/src/main/java/com/uwblueprint/dancefest/CritiqueFormActivity.kt
@@ -0,0 +1,12 @@
+package com.uwblueprint.dancefest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CritiqueFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_critique_form)
+    }
+}
