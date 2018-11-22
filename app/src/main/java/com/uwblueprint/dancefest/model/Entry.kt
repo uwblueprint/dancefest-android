8891dd31d7a70c67e3132f0c519d2bdf0a5279af
Sophie Qin <sophieqin@tdlab.io>
2018-11-22 00:10:38 -0500

    fixed ui for adjudication screen

---
+++ app/src/main/java/com/uwblueprint/dancefest/model/Entry.kt
@@ -0,0 +1,7 @@
+package com.uwblueprint.dancefest.model
+
+class Entry (_danceId: Float, _danceTitle: String, _levelofComp: String, _performers: Array<String>,
+             _danceStyle: String, _School: String, _level: String, _GroupSize: String){
+
+    // properties of 
+}
