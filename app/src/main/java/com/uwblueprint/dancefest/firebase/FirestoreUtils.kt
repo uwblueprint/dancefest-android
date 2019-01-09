feb39af3bc34c7e0ee428cde68ee5a1b3ef37fde
Mark Azer <mark-azer@hotmail.com>
2019-01-08 22:47:51 -0500

    Create add and update data methods

---
+++ app/src/main/java/com/uwblueprint/dancefest/firebase/FirestoreUtils.kt
@@ -0,0 +1,38 @@
+package com.uwblueprint.dancefest.firebase
+
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+
+class FirestoreUtils() {
+    val db = FirebaseFirestore.getInstance()
+    val TAG = "FirestoreUtils"
+
+    init {
+        FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build()
+    }
+
+    fun addData(collectionName: String, data: HashMap<String, Any?>) : Unit {
+        db.collection(collectionName)
+                .add(data)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.id)
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+    }
+
+    fun updateData(collectionName: String, docName: String, data: HashMap<String, Any?>) : Unit {
+        db.collection(collectionName).document(docName)
+                .set(data)
+                .addOnSuccessListener {
+                    Log.d(TAG, "DocumentSnapshot successfully written!")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error writing document", e)
+                }
+    }
+}
