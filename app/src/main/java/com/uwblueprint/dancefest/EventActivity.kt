5faa708b0321ec72c9a4d1edde3425bb8741988e
Daniel Peng <danielpeng2@gmail.com>
2019-01-14 20:16:00 -0500

    get display data from firebase

--- app/src/main/java/com/uwblueprint/dancefest/EventActivity.kt
+++ app/src/main/java/com/uwblueprint/dancefest/EventActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.QuerySnapshot
+import com.uwblueprint.dancefest.firebase.FirestoreUtils
 import kotlinx.android.synthetic.main.activity_event.*
 
 class EventActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var firestoreUtils: FirestoreUtils
+
+    companion object {
+        const val COLLECTION_NAME = "events"
+        const val TAG = "EVENT_ACTIVITY"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,20 +28,31 @@ class EventActivity : AppCompatActivity() {
         // set the title of the action bar
         setTitle(R.string.dancefest)
 
-        // create temporary data
-        val events = arrayListOf(
-                Event("OSSDF2018 - Dance to the Rhythm", "May 22, 2018", "a"),
-                Event("OSSDF2018 - Theme", "Sept 18, 2018", "b"),
-                Event("OSSDF2017 - Theme", "May 21, 2017", "c"),
-                Event("OSSDF2017 - Theme", "Sept 20, 2017", "d"),
-                Event("OSSDF2016 - Theme", "May 14, 2016", "e"))
+        var events: ArrayList<Event>
+
+        firestoreUtils = FirestoreUtils()
+        firestoreUtils.getData(COLLECTION_NAME, EventListener<QuerySnapshot> { value, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed", e)
+                return@EventListener
+            }
+
+            events = arrayListOf()
+
+            for (doc in value!!) {
+                val id = doc.id
+                val data = doc.data
+                events.add(Event(data["eventTitle"] as String, data["eventDate"].toString(), id))
+            }
+
+            viewAdapter = EventsAdapter(events)
+            list_events.apply { adapter = viewAdapter }
+        })
 
         // initialize recyclerview
         viewManager = LinearLayoutManager(this)
-        viewAdapter = EventsAdapter(events)
         list_events.apply {
             layoutManager = viewManager
-            adapter = viewAdapter
         }
     }
 }
