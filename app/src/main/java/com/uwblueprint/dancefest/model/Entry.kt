304f0697d47e2c62a5c6e27e926774f3851983b5
Sophie Qin <sophieq@users.noreply.github.com>
2019-01-19 17:50:14 -0500

    pushes adjudication to firebase

--- app/src/main/java/com/uwblueprint/dancefest/model/Entry.kt
+++
@@ -1,8 +0,0 @@
-package com.uwblueprint.dancefest.model
-
-class Entry (danceId: Double, danceTitle: String, levelofComp: String, performers: Array<String>,
-             danceStyle: String, School: String, level: String, GroupSize: String, artisiticScore: Double = 0.0,
-             technicalScore: Double = 0.0, Notes: String = "",_audioFileId: String = "")
-
-
-
