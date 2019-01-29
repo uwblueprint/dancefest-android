37faa9ae97b90b934ebc9f7ee0a5912021dbf731
Sophie Qin <sophieq@users.noreply.github.com>
2019-01-28 21:37:14 -0500

    applied git stash

--- app/src/main/java/com/uwblueprint/dancefest/CritiqueFormActivity.kt
+++ app/src/main/java/com/uwblueprint/dancefest/CritiqueFormActivity.kt
@@ -4,22 +4,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.ImageButton
 import com.uwblueprint.dancefest.firebase.FirestoreUtils
+import android.support.v7.app.AlertDialog
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_critique_form.*
 
-class CritiqueFormActivity : AppCompatActivity() {
 
-    // TODO: remove keypad on tap
-    // TODO: pull information from firebase for first view
-    // TODO: figure out how to audio field will be displayed (make a view?)
-    // TODO: pop ups for save critique and record new message
-    // TODO: audio recording
+
+class CritiqueFormActivity : AppCompatActivity() {
 
     private lateinit var performance: Performance
     private lateinit var eventId: String
     private lateinit var eventTitle: String
-
+    private lateinit var tabletId: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,19 +24,25 @@ class CritiqueFormActivity : AppCompatActivity() {
         setContentView(R.layout.activity_critique_form)
 
 
+
+        // Current placeholders for information passed from the previous activity.
         eventId = "G25liC0iKFYcZFG3l2d6"
         eventTitle = "OSSDF2018 - Dance to the Rhythm"
+        tabletId = "3"
 
         performance = Performance("1MhtaBW3J0WtoHY3fr3R", "6.0",
-                "Spanish Rose", "6.0_SpanishRose_Competitive_Duet",
+                "Test2", "6.0_SpanishRose_Competitive_Duet",
                 "Paige Docherty, Devyn Kummer", "Musical Theater", "Competitive",
-                "BCI", "Secondary", "Duet")
-
+                "BCI", "Secondary", "Duet", listOf("",""))
 
         populateInfoCard()
 
+
         val btnSave = findViewById<Button>(R.id.saveButton)
         btnSave?.setOnClickListener {
+
+            Log.d("critique_form_activity", "button clicked")
+
 
             val artisticScore = artisticScoreInput.text.toString().toIntOrNull()
             val technicalScore = technicalScoreInput.text.toString().toIntOrNull()
@@ -51,10 +54,13 @@ class CritiqueFormActivity : AppCompatActivity() {
                 cumulativeScore = (artisticScore + technicalScore) / 2
             } else {
                 Log.e("CRITIQUE_ACTIVITY_FORM", "artisticScore or technicalScore is invalid")
-
             }
 
-            val collectionPath = "events/$eventId/performances/${performance.id}/adjudications"
+            Toast.makeText(this@CritiqueFormActivity, "CRITIQUE SAVED", Toast.LENGTH_SHORT).show()
+
+//            val collectionPath = "events/$eventId/performances/${performance.id}/adjudications"
+            val collectionPath = "/events/G25liC0iKFYcZFG3l2d6/performances/1MhtaBW3J0WtoHY3fr3R/adjudications"
+
             Log.d("critique_form_activity", "artistic score is $artisticScore")
             Log.d("critique_form_activity", "collection path is$collectionPath")
 
@@ -64,21 +70,34 @@ class CritiqueFormActivity : AppCompatActivity() {
             data.put("technicalMark", technicalScore)
             data.put("cumulativeScore", cumulativeScore)
             data.put("notes", judgeNotes)
+            data.put("tabletID", tabletId)
 
             FirestoreUtils().addData(collectionPath, data)
+
+            Log.d("critique_form_activity", "data added????")
+
         }
     }
 
     private fun populateInfoCard() {
 
-        danceIDInput.text = performance.entry
-        danceTitleInput.text = performance.title
+        setTitle(R.string.adjudication)
+
+
+        var navPath = "$eventTitle  > ${performance.name}"
+
+        if (navPath.count() >= 60) {
+            navPath.substring(IntRange(0, 60))
+        }
 
         if (performance.performers.count() >= 30) {
             performance.performers.substring(IntRange(0, 30))
         }
 
-        performersInput.text = performance.performers
+        navigationBar.text = "$navPath..."
+        danceIDInput.text = performance.entry
+        danceTitleInput.text = performance.title
+        performersInput.text = "${performance.performers}..."
         danceStyleInput.text = performance.style
         levelOfCompInput.text = performance.levelOfComp
         schoolInput.text = performance.school
--- app/src/main/java/com/uwblueprint/dancefest/Performance.kt
+++ app/src/main/java/com/uwblueprint/dancefest/Performance.kt
@@ -2,4 +2,5 @@ package com.uwblueprint.dancefest
 
 data class Performance(val id: String, val entry: String, val title: String, val name: String,
                        val performers: String, val style: String, val levelOfComp: String,
-                       val school: String,  val level: String, val size: String)
+                       val school: String,  val level: String, val size: String,
+                       val AdjudicationId: List<String>)
